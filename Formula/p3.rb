6b033ce940ea02b375f93b75729195b7db52e20b
taskp3-ci <ci-bot@taskp3.com>
2026-03-05 23:22:14 +0000

    Update p3 to 0.1.11

--- Formula/p3.rb
+++ Formula/p3.rb
@@ -1,8 +1,8 @@
 class P3 < Formula
   desc "TaskP3 CLI - manage tasks from the command line"
   homepage "https://www.taskp3.com/cli"
-  url "https://registry.npmjs.org/@taskp3/cli/-/cli-0.1.10.tgz"
-  sha256 "073a0db1975288b6ad957c39bdb75ac4d0aebbdb4175d93f38d3fac4115fc7bb"
+  url "https://registry.npmjs.org/@taskp3/cli/-/cli-0.1.11.tgz"
+  sha256 "e8ceace1b94af3d52a7e5e1779fab88d37986052d9fb38f69a89ffca5575dba3"
   license "MIT"
 
   depends_on "node"
